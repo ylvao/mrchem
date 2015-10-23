401ea86cae2e9ceb3daa7e1815a346574b6a2a42
Stig Rune Jensen <stig.r.jensen@uit.no>
2015-10-23 13:59:33 +0200

    Move tests to tests/mwrepr.

---
+++ tests/mwrepr/bounding_box.cpp
@@ -0,0 +1,6 @@
+#include "catch.hpp"
+
+#include "BoundingBox.h"
+
+TEST_CASE("BoundingBox", "[bounding_box_constructor], [boxes]") {
+}
---
+++ tests/mwrepr/node_box.cpp
@@ -0,0 +1,7 @@
+#include "catch.hpp"
+
+#include "NodeBox.h"
+
+TEST_CASE("NodeBox constructor", "[node_box_constructor], [boxes]") {
+}
+
