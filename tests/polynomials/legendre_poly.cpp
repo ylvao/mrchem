59e3b16932f48c005aa6e8157697c74a18a6401b
Stig Rune Jensen <stig.r.jensen@uit.no>
2015-10-23 11:59:34 +0200

    Clean up in tests.

--- tests/polynomials/legendre_poly.cpp
+++ tests/polynomials/legendre_poly.cpp
@@ -4,63 +4,35 @@
 
 using namespace std;
 
-SCENARIO("Legendre polynomials can be constructed", "[legendre_constructor], [polynomials]") {
-    GIVEN("the the unscaled Legendre polynomial L_5") {
-        LegendrePoly L_5(5);
-        THEN("the boundaries of L_5 [-1.0, 1.0]") {
-            REQUIRE( L_5.getScaledLowerBound() == Approx(-1.0) );
-            REQUIRE( L_5.getScaledUpperBound() == Approx(1.0) );
-        }
-        THEN("the order of L_5 is 5") {
-            REQUIRE( L_5.getOrder() == 5 );
-        }
-        THEN("the function value L_5(1.0) = 1.0") {
-            REQUIRE( L_5.evalf(1.0) == Approx(1.0) );
-        }
+TEST_CASE("Legendre polynomials", "[legendre_poly], [polynomials]") {
+    int nLeg = 10;
+    vector<LegendrePoly *> L;
+    for (int k = 0; k < nLeg; k++) {
+        LegendrePoly *L_k = new LegendrePoly(k, 2.0, 1.0);
+        L.push_back(L_k);
     }
-    GIVEN("the Legendre polynomial L_3 shifted to the unit interval") {
-        LegendrePoly L_3(3, 2.0, 1.0);
-        THEN("the boundaries of L_3 [0.0, 1.0]") {
-            REQUIRE( L_3.getScaledLowerBound() == Approx(0.0) );
-            REQUIRE( L_3.getScaledUpperBound() == Approx(1.0) );
-        }
-        THEN("the order of L_k is 3") {
-            REQUIRE( L_3.getOrder() == 3 );
-        }
-        THEN("the function value L_3(1.0) = 1.0") {
-            REQUIRE( L_3.evalf(1.0) == Approx(1.0) );
-        }
-    }
-}
 
-SCENARIO("Legendre polynomials are orthogonal", "[legendre_orthogonal], [polynomials]") {
-    GIVEN("the set of 10 lowest order Legendre polynomials") {
-        vector<LegendrePoly *> L;
-        for (int k = 0; k < 10; k++) {
-            LegendrePoly *L_k = new LegendrePoly(k, 2.0, 1.0);
-            L.push_back(L_k);
+    SECTION("LegendrePoly constructor") {
+        for (int k = 0; k < nLeg; k++) {
+            LegendrePoly &L_k = *L[k];
+            REQUIRE( L_k.getScaledLowerBound() == Approx(0.0) );
+            REQUIRE( L_k.getScaledUpperBound() == Approx(1.0) );
+            REQUIRE( L_k.getOrder() == k );
+            // Legendre polynomials are normalized so that L_k(1.0) = 1.0
+            REQUIRE( L_k.evalf(1.0) == Approx(1.0) );
         }
-        THEN("the overlap <L_0|L_0> is non-zero") {
-            LegendrePoly &L_0 = *L[0];
-            double overlap = L_0.innerProduct(L_0);
-            REQUIRE( fabs(overlap) > MachineZero );
-        }
-        THEN("the overlap <L_5|L_5> is non-zero") {
-            LegendrePoly &L_5 = *L[5];
-            double overlap = L_5.innerProduct(L_5);
-            REQUIRE( fabs(overlap) > MachineZero );
-        }
-        THEN("the overlap <L_3|L_2> is zero") {
-            LegendrePoly &L_2 = *L[2];
-            LegendrePoly &L_3 = *L[3];
-            double overlap = L_3.innerProduct(L_2);
-            REQUIRE( fabs(overlap) == Approx(0.0) );
-        }
-        THEN("the overlap <L_1|L_9> is zero") {
-            LegendrePoly &L_1 = *L[1];
-            LegendrePoly &L_9 = *L[9];
-            double overlap = L_1.innerProduct(L_9);
-            REQUIRE( fabs(overlap) == Approx(0.0) );
+    }
+
+    SECTION("The Legendre polynomials form an orthonormal set") {
+        for (int i = 0; i < nLeg; i++) {
+            LegendrePoly &L_i = *L[i];
+            double S_ii = L_i.innerProduct(L_i);
+            REQUIRE( fabs(S_ii) > MachineZero );
+            for (int j = 0; j < i; j++) {
+                LegendrePoly &L_j = *L[j];
+                double S_ij = L_i.innerProduct(L_j);
+                REQUIRE( fabs(S_ij) == Approx(0.0) );
+            }
         }
     }
 }
--- tests/polynomials/polynomial.cpp
+++ tests/polynomials/polynomial.cpp
@@ -4,87 +4,70 @@
 
 using namespace Eigen;
 
-SCENARIO("Polynomials can be constructed", "[poly_constructor], [polynomials]") {
-    GIVEN("a default polynomial P") {
-        Polynomial P;
-        THEN("P is the zero function") {
-            REQUIRE( P.getOrder() == 0 );
-            REQUIRE( P.getCoefs()[0] == Approx(0.0) );
-        }
-        THEN("P is not dilated") {
-            REQUIRE( P.getDilation() == Approx(1.0) );
-        }
-        THEN("P is not translated") {
-            REQUIRE( P.getTranslation() == Approx(0.0) );
-        }
-        THEN("P is not bounded") {
-            REQUIRE_FALSE( P.isBounded() );
-        }
+TEST_CASE("Polynomial constructors", "[poly_constructor], [polynomials]") {
+    double a = 0.0;
+    double b = 2.0;
+    Vector4d c = {0.0, 1.0, 0.0, 0.0};
+    Polynomial P(c, &a, &b);
+
+    SECTION("Polynomial constructor") {
+        REQUIRE( P.getOrder() == 1 );
+        REQUIRE( P.getCoefs()[0] == Approx(0.0) );
+        REQUIRE( P.getDilation() == Approx(1.0) );
+        REQUIRE( P.getTranslation() == Approx(0.0) );
+        REQUIRE( P.isBounded() );
     }
-    GIVEN("A coefficient vector with trailing zeros") {
-        Vector4d c = {0.0, 1.0, 0.0, 0.0};
-        WHEN("A polynomial P is constructed") {
-            Polynomial P(c);
-            THEN("The order of P is given by the highest non-zero coef") {
-                REQUIRE( P.getOrder() == 1 );
-            }
-        }
+
+    SECTION("Copy constructor") {
+        Polynomial Q(P);
+        REQUIRE( Q.getOrder() == 1 );
+        REQUIRE( Q.getCoefs()[0] == Approx(0.0) );
+        REQUIRE( Q.getCoefs()[1] == Approx(1.0) );
+        REQUIRE( Q.getLowerBound(0) == 0.0 );
+        REQUIRE( Q.getUpperBound(0) == 2.0 );
     }
-    GIVEN("The bounded polynomial P(x) = x") {
-        double a = 0.0;
-        double b = 2.0;
-        Vector2d c = {0.0, 1.0};
-        Polynomial P(c, &a, &b);
-        WHEN("the copy constructor is used, Q(P)") {
-            Polynomial Q(P);
-            THEN("Q(x) = x, and Q is bounded") {
-                REQUIRE( Q.getOrder() == 1 );
-                REQUIRE( Q.getCoefs()[0] == Approx(0.0) );
-                REQUIRE( Q.getCoefs()[1] == Approx(1.0) );
-                REQUIRE( Q.getLowerBound(0) == 0.0 );
-                REQUIRE( Q.getUpperBound(0) == 2.0 );
-            }
-        }
-        WHEN("the assignment operator is used, Q = P") {
-            Polynomial Q;
+
+    SECTION("Default constructor") {
+        Polynomial Q;
+        REQUIRE( Q.getOrder() == 0 );
+        REQUIRE( Q.getCoefs()[0] == Approx(0.0) );
+        REQUIRE( Q.getDilation() == Approx(1.0) );
+        REQUIRE( Q.getTranslation() == Approx(0.0) );
+        REQUIRE_FALSE( Q.isBounded() );
+
+        SECTION("Assignment operator") {
             Q = P;
-            THEN("Q(x) = x, but Q is unbounded") {
-                REQUIRE( Q.getOrder() == 1 );
-                REQUIRE( Q.getCoefs()[0] == Approx(0.0) );
-                REQUIRE( Q.getCoefs()[1] == Approx(1.0) );
-                REQUIRE_FALSE( Q.isBounded() );
-            }
+            REQUIRE( Q.getOrder() == 1 );
+            REQUIRE( Q.getCoefs()[0] == Approx(0.0) );
+            REQUIRE( Q.getCoefs()[1] == Approx(1.0) );
+            REQUIRE_FALSE( Q.isBounded() );
         }
     }
 }
 
-SCENARIO("Polynomials can be evaluated", "[poly_evalf], [polynomials]") {
-    GIVEN("a bounded polynomial P") {
-        double a = 0.0;
-        double b = 2.0;
-        Vector3d c = {1.0, 0.0, 2.0};
-        Polynomial P(c, &a, &b);
-        WHEN("P is evaluated within its bounds") {
-            double x = 1.5;
-            double calc_val = P.evalf(&x);
-            THEN("the function value is known") {
-                double ref_val = 1.0 + 2.0*x*x;
-                REQUIRE( calc_val == Approx(ref_val) );
-            }
-        }
-        WHEN("P is evaluated out of bounds") {
-            double x = 2.5;
-            double calc_val = P.evalf(&x);
-            THEN("the function value is zero") {
-                double ref_val = 0.0;
-                REQUIRE( calc_val == Approx(ref_val) );
-            }
-        }
+TEST_CASE("Polynomial evaluation", "[poly_evalf], [polynomials]") {
+    double a = 0.0;
+    double b = 2.0;
+    Vector3d c = {1.0, 0.0, 2.0};
+    Polynomial P(c, &a, &b);
+
+    SECTION("Evaluation within bounds") {
+        double x = 1.5;
+        double calc_val = P.evalf(&x);
+        double ref_val = 1.0 + 2.0*x*x;
+        REQUIRE( calc_val == Approx(ref_val) );
+    }
+
+    SECTION("Evaluation out of bounds") {
+        double x = 2.5;
+        double calc_val = P.evalf(&x);
+        double ref_val = 0.0;
+        REQUIRE( calc_val == Approx(ref_val) );
     }
 }
 
 SCENARIO("Polynomials can be scaled and translated", "[poly_scale], [polynomials]") {
-    GIVEN("a bounded polynomial P") {
+    GIVEN("A bounded polynomial P") {
         double a = -1.0;
         double b = 1.0;
         Vector3d c = {0.0, 1.0, 1.0};
@@ -93,21 +76,21 @@ SCENARIO("Polynomials can be scaled and translated", "[poly_scale], [polynomials
             double n = 2.0;
             double l = 1.0;
             P.rescale(n, l);
-            THEN("the dilation changes") {
+            THEN("The dilation changes") {
                 REQUIRE( P.getDilation() == Approx(2.0) );
             }
-            THEN("the translation changes") {
+            THEN("The translation changes") {
                 REQUIRE( P.getTranslation() == Approx(1.0) );
             }
-            THEN("the scaled bounds change") {
+            THEN("The scaled bounds change") {
                 REQUIRE( P.getScaledLowerBound() == Approx(0.0) );
                 REQUIRE( P.getScaledUpperBound() == Approx(1.0) );
             }
-            THEN("the unscaled bounds don't change") {
+            THEN("The unscaled bounds don't change") {
                 REQUIRE( P.getLowerBound(0) == Approx(-1.0) );
                 REQUIRE( P.getUpperBound(0) == Approx(1.0) );
             }
-            THEN("the scaled evaluation is known") {
+            THEN("The scaled evaluation is known") {
                 double x = 0.3;
                 double calc_val = P.evalf(&x);
                 double ref_val = (2.0*x - 1.0) + (2.0*x - 1.0)*(2.0*x - 1.0);
@@ -118,64 +101,69 @@ SCENARIO("Polynomials can be scaled and translated", "[poly_scale], [polynomials
 }
 
 SCENARIO("Polynomials can be added and multiplied", "[poly_arithmetics], [polynomials]") {
-    GIVEN("two polynomials P and Q") {
-        Vector4d c1 = {0.0, 1.0, 1.0, 0.0};
-        Vector2d c2 = {0.0, 1.0};
-        Polynomial P(c1);
-        Polynomial Q(c2);
+    GIVEN("Two polynomials P and Q") {
+        Vector3d pCoefs = {0.0, 1.0, 1.0};
+        Vector2d qCoefs = {0.0, 1.0};
+        Polynomial P(pCoefs);
+        Polynomial Q(qCoefs);
+
         WHEN("Q += P") {
             Q += P;
-            THEN("the coefficients of P are unchanged") {
+            THEN("The coefficients of P are unchanged") {
                 REQUIRE( P.getOrder() == 2 );
                 REQUIRE( P.getCoefs()[0] == Approx(0.0) );
                 REQUIRE( P.getCoefs()[1] == Approx(1.0) );
                 REQUIRE( P.getCoefs()[2] == Approx(1.0) );
             }
-            THEN("the coefficients of Q change") {
+            THEN("The coefficients of Q change") {
                 REQUIRE( Q.getOrder() == 2 );
                 REQUIRE( Q.getCoefs()[0] == Approx(0.0) );
                 REQUIRE( Q.getCoefs()[1] == Approx(2.0) );
                 REQUIRE( Q.getCoefs()[2] == Approx(1.0) );
             }
         }
+
         WHEN("R = P + Q") {
             Polynomial R;
             R = P+Q;
-            THEN("the coefficients of R are known") {
+            THEN("The coefficients of R are known") {
                 REQUIRE( R.getOrder() == 2 );
                 REQUIRE( R.getCoefs()[0] == Approx(0.0) );
                 REQUIRE( R.getCoefs()[1] == Approx(2.0) );
                 REQUIRE( R.getCoefs()[2] == Approx(1.0) );
             }
         }
+
         WHEN("P *= 2.0") {
             P *= 2.0;
-            THEN("the coefficients of P change") {
+            THEN("The coefficients of P change") {
                 REQUIRE( P.getOrder() == 2 );
                 REQUIRE( P.getCoefs()[0] == Approx(0.0) );
                 REQUIRE( P.getCoefs()[1] == Approx(2.0) );
                 REQUIRE( P.getCoefs()[2] == Approx(2.0) );
             }
         }
+
         WHEN("R = P*3.0") {
             Polynomial R;
             R = P*3.0;
-            THEN("the coefficients of R are known") {
+            THEN("The coefficients of R are known") {
                 REQUIRE( R.getOrder() == 2 );
                 REQUIRE( R.getCoefs()[0] == Approx(0.0) );
                 REQUIRE( R.getCoefs()[1] == Approx(3.0) );
                 REQUIRE( R.getCoefs()[2] == Approx(3.0) );
             }
         }
+
         WHEN("Q *= P") {
             Q *= P;
-            THEN("the coefficients of P are unchanged") {
+            THEN("The coefficients of P are unchanged") {
                 REQUIRE( P.getOrder() == 2 );
                 REQUIRE( P.getCoefs()[0] == Approx(0.0) );
                 REQUIRE( P.getCoefs()[1] == Approx(1.0) );
                 REQUIRE( P.getCoefs()[2] == Approx(1.0) );
             }
-            THEN("the coefficients of Q change") {
+            THEN("The coefficients of Q change") {
                 REQUIRE( Q.getOrder() == 3 );
                 REQUIRE( Q.getCoefs()[0] == Approx(0.0) );
                 REQUIRE( Q.getCoefs()[1] == Approx(0.0) );
@@ -183,10 +171,11 @@ SCENARIO("Polynomials can be added and multiplied", "[poly_arithmetics], [polyno
                 REQUIRE( Q.getCoefs()[3] == Approx(1.0) );
             }
         }
+
         WHEN("R = P * Q") {
             Polynomial R;
             R = P*Q;
-            THEN("the coefficients of R are known") {
+            THEN("The coefficients of R are known") {
                 VectorXd &cr = R.getCoefs();
                 REQUIRE( R.getCoefs()[0] == Approx(0.0) );
                 REQUIRE( R.getCoefs()[1] == Approx(0.0) );
@@ -197,67 +186,84 @@ SCENARIO("Polynomials can be added and multiplied", "[poly_arithmetics], [polyno
     }
 }
 
-SCENARIO("Polynomials can be differentiated and integrated", "[poly_diff], [polynomials]") {
-    GIVEN("a polynomial P") {
-        Vector3d c = {0.0, 1.0, 2.0};
-        Polynomial P(c);
-        WHEN("Q is the derivative of P") {
-            Polynomial Q = P.calcDerivative();
-            THEN("the coefficients of Q are known") {
-                REQUIRE( Q.getOrder() == 1 );
-                REQUIRE( Q.getCoefs()[0] == Approx(1.0) );
-                REQUIRE( Q.getCoefs()[1] == Approx(4.0) );
-            }
-        }
-        WHEN("Q is the anti derivative of P") {
-            Polynomial Q = P.calcAntiDerivative();
-            THEN("the coefficients of Q are known") {
-                REQUIRE( Q.getOrder() == 3 );
-                REQUIRE( Q.getCoefs()[0] == Approx(0.0) );
-                REQUIRE( Q.getCoefs()[1] == Approx(0.0) );
-                REQUIRE( Q.getCoefs()[2] == Approx(1.0/2.0) );
-                REQUIRE( Q.getCoefs()[3] == Approx(2.0/3.0) );
-            }
-        }
+TEST_CASE("Polynomial differentiation", "[poly_diff], [polynomials]") {
+    Vector3d c = {0.0, 1.0, 2.0};
+    Polynomial P(c);
+
+    SECTION("Derivative in place") {
+        P.calcDerivativeInPlace();
+        REQUIRE( P.getOrder() == 1 );
+        REQUIRE( P.getCoefs()[0] == Approx(1.0) );
+        REQUIRE( P.getCoefs()[1] == Approx(4.0) );
     }
-    GIVEN("The bounded polynomial P on [-1.0, 1.0]") {
+
+    SECTION("Derivative") {
+        Polynomial Q = P.calcDerivative();
+        REQUIRE( Q.getOrder() == 1 );
+        REQUIRE( Q.getCoefs()[0] == Approx(1.0) );
+        REQUIRE( Q.getCoefs()[1] == Approx(4.0) );
+    }
+}
+
+TEST_CASE("Polynomial integration", "[poly_int], [polynomials]") {
+    Vector3d c = {0.0, 1.0, 2.0};
+    Polynomial P(c);
+
+    SECTION("Antiderivative in place") {
+        P.calcAntiDerivativeInPlace();
+        REQUIRE( P.getOrder() == 3 );
+        REQUIRE( P.getCoefs()[0] == Approx(0.0) );
+        REQUIRE( P.getCoefs()[1] == Approx(0.0) );
+        REQUIRE( P.getCoefs()[2] == Approx(1.0/2.0) );
+        REQUIRE( P.getCoefs()[3] == Approx(2.0/3.0) );
+    }
+
+    SECTION("Antiderivative") {
+        Polynomial Q = P.calcAntiDerivative();
+        REQUIRE( Q.getOrder() == 3 );
+        REQUIRE( Q.getCoefs()[0] == Approx(0.0) );
+        REQUIRE( Q.getCoefs()[1] == Approx(0.0) );
+        REQUIRE( Q.getCoefs()[2] == Approx(1.0/2.0) );
+        REQUIRE( Q.getCoefs()[3] == Approx(2.0/3.0) );
+    }
+
+    GIVEN("A bounded polynomial P on [-1.0, 1.0]") {
         double a = -1.0;
         double b = 1.0;
-        Vector3d c = {0.0, 1.0, 1.0};
-        Polynomial P(c, &a, &b);
+        P.setBounds(&a, &b);
         THEN("P can be integrated on its full domain") {
             double calc_int = P.integrate();
-            double ref_int = 2.0/3.0;
+            double ref_int = 4.0/3.0;
             REQUIRE( calc_int == Approx(ref_int) );
         }
-        THEN("P can be integrated on a subdomain [0.0, 1.0]") {
+        THEN("P can be integrated on the subdomain [0.0, 1.0]") {
             a = 0.0;
             double calc_int = P.integrate(&a, &b);
-            double ref_int = 5.0/6.0;
+            double ref_int = 7.0/6.0;
             REQUIRE( calc_int == Approx(ref_int) );
         }
     }
 }
 
 SCENARIO("Bounded polynomials have inner products and norms", "[poly_norm], [polynomials]") {
-    GIVEN("two unbounded polynomials P and Q") {
+    GIVEN("Two unbounded polynomials P and Q") {
         Vector3d c1 = {0.0, 1.0, 1.0};
         Vector2d c2 = {0.0, 1.0};
         Polynomial P(c1);
         Polynomial Q(c2);
-        THEN("the norm of P is undefined") {
+        THEN("The norm of P is undefined") {
             REQUIRE( P.calcSquareNorm() < 0.0 );
         }
         WHEN("P is bounded") {
             double a = -1.0;
             double b = 1.0;
             P.setBounds(&a, &b);
-            THEN("the inner product <P|Q> is defined") {
+            THEN("The inner product <P|Q> is defined") {
                 double ref_inner = 2.0/3.0;
                 double calc_inner = P.innerProduct(Q);
                 REQUIRE( calc_inner == Approx(ref_inner) );
             }
-            THEN("the norm of P is defined") {
+            THEN("The norm of P is defined") {
                 double ref_norm = 16.0/15.0;
                 double calc_norm = P.calcSquareNorm();
                 REQUIRE( calc_norm == Approx(ref_norm) );
