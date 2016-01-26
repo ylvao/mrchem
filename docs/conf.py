ce777715435b9cf05f14bdb8bf55dae2f4fcf028
Roberto Di Remigio <roberto.d.remigio@uit.no>
2016-01-26 15:24:05 +0100

    Add requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ import shlex
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
+    'sphinxcontrib.bibtex'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,7 +41,16 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+# Markdown support
+
+from recommonmark.parser import CommonMarkParser
+
+# The suffix of source filenames.
+source_suffix = ['.rst', '.md']
+
+source_parsers = {
+            '.md': CommonMarkParser,
+            }
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
@@ -111,7 +121,9 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
