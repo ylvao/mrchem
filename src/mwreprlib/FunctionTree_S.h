e58232062900d418e7952d05ff1b6fbae0f7a296
Stig Rune Jensen <stig.r.jensen@uit.no>
2016-02-09 19:33:47 +0100

    Remove unused stuff from FunctionTree_S.

--- src/mwreprlib/FunctionTree_S.h
+++ src/mwreprlib/FunctionTree_S.h
@@ -21,37 +21,11 @@ template<int D>
 class FunctionTree_S  {
 public:
     FunctionTree_S(const MultiResolutionAnalysis<D> &mra, int maxNumberOfNodes);
-    FunctionTree_S(const FunctionTree_S<D> &tree);
-    FunctionTree_S<D> &operator=(const FunctionTree_S<D> &tree);
     virtual ~FunctionTree_S();
 
-    void clear();
-
-    double integrate() const;
-    virtual double dot(const FunctionTree_S<D> &ket);
-    virtual double evalf(const double *r);
-
-    // In place operations
-    void square();
-    void power(double d);
-    void normalize();
-    void orthogonalize(const FunctionTree_S<D> &tree);
-
-    FunctionTree_S<D>& operator *=(double c);
-    FunctionTree_S<D>& operator *=(const FunctionTree_S<D> &tree);
-    FunctionTree_S<D>& operator +=(const FunctionTree_S<D> &tree);
-    FunctionTree_S<D>& operator -=(const FunctionTree_S<D> &tree);
-
-    FunctionNode<D> &getEndFuncNode(int i) { return static_cast<FunctionNode<D> &>(this->funcTree_p->getEndNode(i)); }
-    FunctionNode<D> &getRootFuncNode(int i) { return static_cast<FunctionNode<D> &>(this->funcTree_p->rootBox.getNode(i)); }
-
-    const FunctionNode<D> &getEndFuncNode(int i) const { return static_cast<const FunctionNode<D> &>(this->funcTree_p->getEndNode(i)); }
-    const FunctionNode<D> &getRootFuncNode(int i) const { return static_cast<const FunctionNode<D> &>(this->funcTree_p->rootBox.getNode(i)); }
+    FunctionTree<D> &getTree() { return *this->funcTree_p; }
 
 protected:
-    // Static default parameters
-    const static int tDim = (1 << D);
-
     int sizeTreeMeta; //The first part of the Tree is filled with metadata; reserved size:
     int sizeNode;     //The dynamical part of the tree is filled with nodes of size:
     int maxNodes;     //max number of nodes that can be defined
