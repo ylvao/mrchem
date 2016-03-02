341d2fe95ae0c5d86d1e009f8cdc110545c92a40
Stig Rune Jensen <stig.r.jensen@uit.no>
2016-03-02 17:17:28 +0100

    Fix memory leak GridCleaner

--- src/mwreprlib/GridCleaner.h
+++ src/mwreprlib/GridCleaner.h
@@ -10,12 +10,14 @@ public:
             : TreeBuilder<D>(mra, -1) {
         this->calculator = new TreeCalculator<D>();
     }
+    
     GridCleaner(const MultiResolutionAnalysis<D> &mra,
                 const TreeAdaptor<D> &a)
             : TreeBuilder<D>(mra, -1) {
         this->calculator = new TreeCalculator<D>();
         this->adaptor = a.copy();
     }
+
     virtual ~GridCleaner() {
         this->clearCalculator();
         this->clearAdaptor();
@@ -26,26 +28,29 @@ public:
     }
 
 protected:
+
     int clean(MWTree<D> &tree) {
         if (this->calculator == 0) MSG_ERROR("Calculator not initialized");
         println(10, " == Clearing tree");
 
         int nSplit = 0;
         if (this->adaptor != 0) {
-            MWNodeVector *endVec = tree.copyEndNodeTable();
-            endVec = this->clearForeignNodes(endVec);
-            MWNodeVector *splitVec = this->adaptor->splitNodeVector(*endVec);
+            MWNodeVector *workVec = tree.copyEndNodeTable();
+            workVec = this->clearForeignNodes(workVec);
+            MWNodeVector *splitVec = this->adaptor->splitNodeVector(*workVec);
             NodeIndexSet *splitSet = this->getNodeIndexSet(*splitVec);
             nSplit = splitVec->size();
 
             broadcast_index_list<D>(*splitSet);
             tree.splitNodes(*splitSet);//allocate new nodes
 
+            delete workVec;
             delete splitVec;
             delete splitSet;
         }
 
-        printout(10, "  -- #  0: Split        " << std::setw(6) << nSplit << " nodes\n");
+        printout(10, "  -- #  0: Split        ");
+        printout(10, std::setw(6) << nSplit << " nodes\n");
         printout(10, "  -- #  1: Cleared      ");
 
         MWNodeVector nodeVec;
