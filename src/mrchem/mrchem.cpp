35bd4e476bec296bc76e31618a71b57472ec611f
Stig Rune Jensen <stig.r.jensen@uit.no>
2016-03-14 13:01:18 +0100

    Optimize and OpenMP parallelize TreeBuilders
    MRCPP is only OpenMP parallelized for the time being
    (maybe permanently). New Timer class based on the proper
    boost/timer/timer.hpp (instead of the depricated
    boost/timer.hpp) timer.

---
+++ src/mrchem/mrchem.cpp
@@ -0,0 +1,27 @@
+/** \mainpage The MRChem program
+ *
+ * \author Stig Rune Jensen
+ *
+ * \version 1.0
+ *
+ * \par Copyright:
+ * GPLv4
+ *
+ */
+
+#include <Eigen/Core>
+
+#include "MREnv.h"
+
+int main(int argc, char **argv) {
+    mpi::environment env(argc, argv);
+
+    Timer rolex;
+    rolex.restart();
+
+    MREnv::initializeMRCPP();
+    MREnv::finalizeMRCPP(rolex);
+
+    return 0;
+}
+
