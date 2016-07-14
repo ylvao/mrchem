1481ad69227b5fcae7ab5218d2043920740265d0
Stig Rune Jensen <stig.r.jensen@uit.no>
2016-07-14 12:57:51 +0200

    Remove FortranFlags from ConfigCompilerFlags

--- cmake/ConfigCompilerFlags.cmake
+++ cmake/ConfigCompilerFlags.cmake
@@ -8,10 +8,6 @@ if (CMAKE_CXX_COMPILER_WORKS)
     include(${PROJECT_SOURCE_DIR}/cmake/compilers/CXXFlags.cmake)
 endif()
 
-if (CMAKE_Fortran_COMPILER_WORKS)
-    include(${PROJECT_SOURCE_DIR}/cmake/compilers/FortranFlags.cmake)
-endif()
-
 if (CMAKE_C_COMPILER_ID MATCHES Intel OR
         CMAKE_CXX_COMPILER_ID MATCHES Intel OR
         CMAKE_Fortran_COMPILER_ID MATCHES Intel)
