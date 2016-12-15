a4718f82e1590b5c99d1e9d5e5a9a7ad7c3e0aeb
Stig Rune Jensen <stig.r.jensen@uit.no>
2016-12-15 13:37:10 +0100

    change Vector3d -> VectorXd in property

--- src/mrchem/properties/DipoleMoment.h
+++ src/mrchem/properties/DipoleMoment.h
@@ -5,19 +5,18 @@
 
 #include "TelePrompter.h"
 #include "OrbitalVector.h"
-#include "DipoleOperator.h"
 
 class DipoleMoment {
 public:
     DipoleMoment() {
-        this->nuclear.setZero();
-        this->electronic.setZero();
+        this->nuclear = Eigen::VectorXd::Zero(3);
+        this->electronic = Eigen::VectorXd::Zero(3);
     }
     virtual ~DipoleMoment() { }
 
-    Eigen::Vector3d get() const { return this->nuclear + this->electronic; }
-    Eigen::Vector3d& getNuclear() { return this->nuclear; }
-    Eigen::Vector3d& getElectronic() { return this->electronic; }
+    Eigen::VectorXd get() const { return this->nuclear + this->electronic; }
+    Eigen::VectorXd& getNuclear() { return this->nuclear; }
+    Eigen::VectorXd& getElectronic() { return this->electronic; }
 
     friend std::ostream& operator<<(std::ostream &o, const DipoleMoment &dipole) {
         int oldPrec = TelePrompter::setPrecision(10);
@@ -39,8 +38,8 @@ public:
         return o;
     }
 protected:
-    Eigen::Vector3d nuclear;
-    Eigen::Vector3d electronic;
+    Eigen::VectorXd nuclear;
+    Eigen::VectorXd electronic;
 };
 
 #endif // DIPOLEMOMENT_H
