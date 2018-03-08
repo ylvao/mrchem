440add9805811d8cba9e64f284a26635afd9b59e
Stig Rune Jensen <stig.r.jensen@uit.no>
2018-03-08 18:12:10 +0100

    Add QMFunction class

---
+++ src/qmfunctions/QMFunction.cpp
@@ -0,0 +1,58 @@
+#include "MRCPP/Printer"
+
+#include "QMFunction.h"
+
+namespace mrchem {
+extern mrcpp::MultiResolutionAnalysis<3> *MRA; // Global MRA
+
+QMFunction& QMFunction::operator=(const QMFunction &func) {
+    if (this != &func) {
+        this->re = func.re;
+        this->im = func.im;
+    }
+    return *this;
+}
+
+void QMFunction::alloc(int type) {
+    if (type == NUMBER::Real or type == NUMBER::Total) {
+        if (this->hasReal()) MSG_FATAL("Function not empty");
+        this->re = new mrcpp::FunctionTree<3>(*MRA);
+    }
+    if (type == NUMBER::Imag or type == NUMBER::Total) {
+        if (this->hasImag()) MSG_FATAL("Function not empty");
+        this->im = new mrcpp::FunctionTree<3>(*MRA);
+    }
+}
+
+void QMFunction::clear(int type) {
+    if (type == NUMBER::Real or type == NUMBER::Total) {
+        this->re = 0;
+    }
+    if (type == NUMBER::Imag or type == NUMBER::Total) {
+        this->im = 0;
+    }
+}
+
+void QMFunction::free(int type) {
+    if (type == NUMBER::Real or type == NUMBER::Total) {
+        if (this->hasReal()) delete this->re;
+        this->re = 0;
+    }
+    if (type == NUMBER::Imag or type == NUMBER::Total) {
+        if (this->hasImag()) delete this->im;
+        this->im = 0;
+    }
+}
+
+int QMFunction::getNNodes(int type) const {
+    int nNodes = 0;
+    if (type == NUMBER::Real or type == NUMBER::Total) {
+        if (this->hasReal()) nNodes += this->real().getNNodes();
+    }
+    if (type == NUMBER::Imag or type == NUMBER::Total) {
+        if (this->hasImag()) nNodes += this->imag().getNNodes();
+    }
+    return nNodes;
+}
+
+} //namespace mrchem
---
+++ src/qmfunctions/QMFunction.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "qmfunctions.h"
+
+namespace mrchem {
+
+class QMFunction {
+public:
+    QMFunction(mrcpp::FunctionTree<3> *r = 0, mrcpp::FunctionTree<3> *i = 0) : re(r), im(i) { }
+    QMFunction(const QMFunction &func) : re(func.re), im(func.im) { }
+    QMFunction &operator=(const QMFunction &func);
+    virtual ~QMFunction() { }
+
+    void alloc(int type = NUMBER::Total);
+    void clear(int type = NUMBER::Total);
+    void free(int type = NUMBER::Total);
+
+    int getNNodes(int type = NUMBER::Total) const;
+
+    bool hasReal() const { return (this->re == 0) ? false : true; }
+    bool hasImag() const { return (this->im == 0) ? false : true; }
+
+    mrcpp::FunctionTree<3> &real() { return *this->re; }
+    mrcpp::FunctionTree<3> &imag() { return *this->im; }
+
+    const mrcpp::FunctionTree<3> &real() const { return *this->re; }
+    const mrcpp::FunctionTree<3> &imag() const { return *this->im; }
+
+    void setReal(mrcpp::FunctionTree<3> *real) { this->re = real; }
+    void setImag(mrcpp::FunctionTree<3> *imag) { this->im = imag; }
+
+protected:
+    mrcpp::FunctionTree<3> *re;     ///< Real part of function
+    mrcpp::FunctionTree<3> *im;     ///< Imaginary part of function
+};
+
+} // namespace mrchem
