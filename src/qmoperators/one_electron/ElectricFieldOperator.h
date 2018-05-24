7bd7fef31a689f5137c481d5356870d5bccb832e
Luca Frediani <luca.frediani@uit.no>
2018-05-24 09:48:05 +0200

    First version of electric and magnetic field operators

---
+++ src/qmoperators/one_electron/ElectricFieldOperator.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "RankZeroTensorOperator.h"
+#include "H_E_dip.h"
+
+/** @class ElectricFieldOperator
+ *
+ * @brief External electric field operator
+ *
+ * An external electric field interacts with the molecular dipole
+ * moment. The operator is simply implemented as a scalar product of
+ * the dipole moment operator with the electric field vector.
+ *
+ */
+
+namespace mrchem {
+
+class ElectricFieldOperator final : public RankZeroTensorOperator {
+public:
+ ElectricFieldOperator(Eigen::Vector3D f)
+     : field(f) {
+        RankZeroTensorOperator &d_x = this->dipole[0];
+        RankZeroTensorOperator &d_y = this->dipole[1];
+        RankZeroTensorOperator &d_z = this->dipole[2];
+
+        RankZeroTensorOperator &HEF = (*this);
+        HEF = 0.5*(f[0]*d_x + f[1]*d_y + f[2]*d_z);
+    }
+
+protected:
+    Eigen::Vector3D field;
+    H_E_dip dipole;
+    
+};
+
+} //namespace mrchem
---
+++ src/qmoperators/one_electron/MagneticFieldOperator.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "RankZeroTensorOperator.h"
+#include "H_B_dip.h"
+
+/** @class MagneticFieldOperator
+ *
+ * @brief External magnetic field operator
+ *
+ * An external magnetic field interacts with the molecular dipole
+ * moment. The operator is simply implemented as a scalar product of
+ * the dipole moment operator with the magnetic field vector.
+ *
+ */
+
+namespace mrchem {
+
+class MagneticFieldOperator final : public RankZeroTensorOperator {
+public:
+ MagneticFieldOperator(Eigen::Vector3D f)
+     : field(f) {
+        RankZeroTensorOperator &d_x = this->dipole[0];
+        RankZeroTensorOperator &d_y = this->dipole[1];
+        RankZeroTensorOperator &d_z = this->dipole[2];
+
+        RankZeroTensorOperator &HMF = (*this);
+        HMF = 0.5*(f[0]*d_x + f[1]*d_y + f[2]*d_z);
+    }
+
+protected:
+    Eigen::Vector3D field;
+    H_B_dip dipole;
+    
+};
+
+} //namespace mrchem
