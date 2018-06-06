db749b3ab67d093212fcc9196cc182d12f310cc8
Luca Frediani <luca.frediani@uit.no>
2018-06-06 12:45:34 +0200

    Correct sign of the dipole moment
    1) Now two dipole moments (exp. value and finite differences) agree.
    2) I don't manage to make the sign of the nuclear and electronic part consistent (aesthetic issue)
    3) I assume there is a simpler way to implement the sign change in H_E_dip than what I have done.

--- src/qmoperators/one_electron/H_E_dip.h
+++ src/qmoperators/one_electron/H_E_dip.h
@@ -7,7 +7,15 @@ namespace mrchem {
 
 class H_E_dip final : public PositionOperator {
 public:
-    H_E_dip(const double *o = 0) : PositionOperator(o) { }
+    H_E_dip(const double *o = 0) : PositionOperator(o) { 
+        RankOneTensorOperator<3> &h = (*this);
+        h[0] = (*this)[0];
+        h[1] = (*this)[1];
+        h[2] = (*this)[2];
+        h[0] = -1.0 * h[0];
+        h[1] = -1.0 * h[1];
+        h[2] = -1.0 * h[2];
+    }
     ~H_E_dip() { }
 
     ComplexVector trace(const Nuclei &nucs) {
@@ -21,9 +29,9 @@ public:
         ComplexVector result = ComplexVector::Zero(3);
         double Z = nuc.getCharge();
         const double *R = nuc.getCoord();
-        result(0) = -Z*this->r_x.real().evalf(R);
-        result(1) = -Z*this->r_y.real().evalf(R);
-        result(2) = -Z*this->r_z.real().evalf(R);
+        result(0) = Z*this->r_x.real().evalf(R);
+        result(1) = Z*this->r_y.real().evalf(R);
+        result(2) = Z*this->r_z.real().evalf(R);
         return result;
     }
 
