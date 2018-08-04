7ea0e355daf397f4f0d9bf4bf9b638bc6e852e40
Luca Frediani <luca.frediani@uit.no>
2018-08-04 15:45:29 +0200

    Compiles

--- tests/qmfunctions/density.cpp
+++ tests/qmfunctions/density.cpp
@@ -3,6 +3,7 @@
 #include "mrchem.h"
 #include "Orbital.h"
 #include "HydrogenFunction.h"
+#include "Density.h"
 
 using namespace mrchem;
 
@@ -13,7 +14,7 @@ TEST_CASE("Density", "[density]") {
     const double thrs = 1.0e-12;
 
     SECTION("calc density") {
-        Density rho(*MRA);
+        Density rho;
 
         SECTION("single orbital") {
             HydrogenFunction h(1,0,0);
@@ -21,9 +22,9 @@ TEST_CASE("Density", "[density]") {
             phi.alloc(NUMBER::Real);
             mrcpp::project(prec, phi.real(), h);
 
-            mrcpp::build_grid(rho, phi.real());
+            mrcpp::build_grid(rho.real(), phi.real());
             density::compute(-1.0, rho, phi, DENSITY::Total);
-            REQUIRE( rho.integrate() == Approx(1.0) );
+            REQUIRE( rho.real().integrate() == Approx(1.0) );
 
             phi.free();
         }
@@ -44,17 +45,17 @@ TEST_CASE("Density", "[density]") {
             mrcpp::project(prec, Phi[2].imag(), h_3);
 
             density::compute(prec, rho, Phi, DENSITY::Total);
-            REQUIRE( rho.integrate() == Approx(4.0) );
+            REQUIRE( rho.real().integrate() == Approx(4.0) );
 
             orbital::free(Phi);
         }
     }
 
     SECTION("calc spin density") {
-        Density rho_t(*MRA);
-        Density rho_s(*MRA);
-        Density rho_a(*MRA);
-        Density rho_b(*MRA);
+        Density rho_t;
+        Density rho_s;
+        Density rho_a;
+        Density rho_b;
 
         SECTION("single orbital") {
             HydrogenFunction h(1,0,0);
@@ -62,19 +63,19 @@ TEST_CASE("Density", "[density]") {
             phi.alloc(NUMBER::Real);
             mrcpp::project(prec, phi.real(), h);
 
-            mrcpp::build_grid(rho_t, phi.real());
-            mrcpp::build_grid(rho_s, phi.real());
-            mrcpp::build_grid(rho_a, phi.real());
-            mrcpp::build_grid(rho_b, phi.real());
+            mrcpp::build_grid(rho_t.real(), phi.real());
+            mrcpp::build_grid(rho_s.real(), phi.real());
+            mrcpp::build_grid(rho_a.real(), phi.real());
+            mrcpp::build_grid(rho_b.real(), phi.real());
             density::compute(-1.0, rho_t, phi, DENSITY::Total);
             density::compute(-1.0, rho_s, phi, DENSITY::Spin);
             density::compute(-1.0, rho_a, phi, DENSITY::Alpha);
             density::compute(-1.0, rho_b, phi, DENSITY::Beta);
 
-            REQUIRE( rho_t.integrate() == Approx(1.0) );
-            REQUIRE( rho_s.integrate() == Approx(1.0) );
-            REQUIRE( rho_a.integrate() == Approx(1.0) );
-            REQUIRE( rho_b.integrate() == Approx(0.0) );
+            REQUIRE( rho_t.real().integrate() == Approx(1.0) );
+            REQUIRE( rho_s.real().integrate() == Approx(1.0) );
+            REQUIRE( rho_a.real().integrate() == Approx(1.0) );
+            REQUIRE( rho_b.real().integrate() == Approx(0.0) );
 
             phi.free();
         }
@@ -115,10 +116,10 @@ TEST_CASE("Density", "[density]") {
             density::compute(prec, rho_a, Phi, DENSITY::Alpha);
             density::compute(prec, rho_b, Phi, DENSITY::Beta);
 
-            REQUIRE( rho_t.integrate() == Approx(7.0) );
-            REQUIRE( rho_s.integrate() == Approx(3.0) );
-            REQUIRE( rho_a.integrate() == Approx(5.0) );
-            REQUIRE( rho_b.integrate() == Approx(2.0) );
+            REQUIRE( rho_t.real().integrate() == Approx(7.0) );
+            REQUIRE( rho_s.real().integrate() == Approx(3.0) );
+            REQUIRE( rho_a.real().integrate() == Approx(5.0) );
+            REQUIRE( rho_b.real().integrate() == Approx(2.0) );
 
             orbital::free(Phi);
         }
