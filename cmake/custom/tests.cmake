91aade2414e06895bc8f79a5c0fcab9fe7678d52
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-09-13 10:28:44 -0400

    Tests are always available

--- cmake/custom/tests.cmake
+++ cmake/custom/tests.cmake
@@ -1,21 +1,5 @@
-#.rst:
-#
-# autocmake.yml configuration::
-#
-#   docopt:
-#     - "--enable-tests Enable integration tests for mrchem.x [default: False]."
-#     - "--enable-unit-tests Enable unit tests for libmrchem.a [default: False]."
-#   define:
-#     - "'-DENABLE_TESTS={0}'.format(arguments['--enable-tests'])"
-#     - "'-DENABLE_UNIT_TESTS={0}'.format(arguments['--enable-unit-tests'])"
-
-option(ENABLE_TESTS "Enable test suite" ON)
-option(ENABLE_UNIT_TESTS "Enable test suite" ON)
-
 include(testing_macros)
 
-if(ENABLE_UNIT_TESTS OR ENABLE_TESTS)
-  enable_testing()
-  include(CTest)
-  add_subdirectory(tests) # This must come last!!
-endif()
+enable_testing()
+include(CTest)
+add_subdirectory(tests) # This must come last!!
