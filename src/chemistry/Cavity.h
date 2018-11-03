81e4a7fb20182e9366d070deb23abefb0880aff8
Gabriel Gerez <gabrielgerez@stallo-1.local>
2018-11-03 15:11:09 +0100

    fix evalf

--- src/chemistry/Cavity.h
+++ src/chemistry/Cavity.h
@@ -2,14 +2,16 @@
 #include <array>
 #include "MRCPP/MWFunctions"
 
+using namespace mrcpp;
+
 namespace mrchem {
 
 
 class Cavity : public mrcpp::RepresentableFunction<3> {
 public: 
   Cavity(std::vector<std::array<double, 3>> coord, std::vector<double> R, double slope);
-  double evalf(const double *r) const;
-  double evalf(const std::array<double, 3> &r) const {return 0;}
+  double evalf(const double *r) const override; 
+  double evalf(const std::array<double, 3> &r) const {return evalf(r.data());};
 protected:
   std::vector<std::array<double, 3>> pos;
   std::vector<double> R;
