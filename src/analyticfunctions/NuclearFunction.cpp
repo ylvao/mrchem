27263c3b749387a831eba4881b730b863ec4e342
Luca Frediani <luca.frediani@uit.no>
2018-11-20 10:42:25 +0100

    Update NuclearFunction.cpp

--- src/analyticfunctions/NuclearFunction.cpp
+++ src/analyticfunctions/NuclearFunction.cpp
@@ -48,7 +48,7 @@ double NuclearFunction::evalf(const mrcpp::Coord<3> &r) const {
     return result;
 }
 
-bool NuclearFunction::isVisibleAtScale(int scale, int nQuadPts) const override {
+bool NuclearFunction::isVisibleAtScale(int scale, int nQuadPts) const {
     double minSmooth = this->smooth[0];
     for (int i = 1; i < this->smooth.size(); i++) {
         if (this->smooth[i] < minSmooth) {
@@ -64,7 +64,7 @@ bool NuclearFunction::isVisibleAtScale(int scale, int nQuadPts) const override {
     }
 }
 
-bool NuclearFunction::isZeroOnInterval(const double *a, const double *b) const override {
+bool NuclearFunction::isZeroOnInterval(const double *a, const double *b) const {
     int totSplit = 0;
     for (int i = 0; i < this->nuclei.size(); i++) {
         const mrcpp::Coord<3> &R = this->nuclei[i].getCoord();
