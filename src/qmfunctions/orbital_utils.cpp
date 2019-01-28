1098efcdf57a3a25e29bbd742559e631d69c8187
Peter Wind <peter.wind@met.no>
2019-01-28 15:47:25 +0100

    git hooks

--- src/qmfunctions/orbital_utils.cpp
+++ src/qmfunctions/orbital_utils.cpp
@@ -2,7 +2,7 @@
  * MRChem, a numerical real-space code for molecular electronic structure
  * calculations within the self-consistent field (SCF) approximations of quantum
  * chemistry (Hartree-Fock and Density Functional Theory).
- * Copyright (C) 2019 Stig Rune Jensen, Jonas Juselius, Luca Frediani, and contributors.
+ * Copyright (C) 2019 Stig Rune Jensen, Luca Frediani, Peter Wind and contributors.
  *
  * This file is part of MRChem.
  *
@@ -25,7 +25,6 @@
 
 #include "MRCPP/Printer"
 #include "MRCPP/Timer"
-#include "MRCPP/trees/SerialTree.h"
 
 #include "parallel.h"
 #include "utils/RRMaximizer.h"
