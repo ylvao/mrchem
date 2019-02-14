1e57981e3ad2b09d012bc277bb834c7d1ee2b8cf
Stig Rune Jensen <stig.r.jensen@uit.no>
2019-02-14 18:11:22 +0100

    Update getkw

--- external/upstream/fetch_getkw.cmake
+++ external/upstream/fetch_getkw.cmake
@@ -11,7 +11,7 @@ else()
     GIT_REPOSITORY
       https://github.com/dev-cafe/libgetkw.git
     GIT_TAG
-      986403e1f9b50210d6e45bb4fa336f08ddd31960 # Preferable to have a tag for a release
+      b0174c4c1b8df187fbabe34db47160eedf72faca # Preferable to have a tag for a release
     CMAKE_ARGS
       -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
       -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
