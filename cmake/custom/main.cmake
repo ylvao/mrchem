533b20f64ce2c919668d43375960185398ec458e
Stig Rune Jensen <stig.r.jensen@uit.no>
2019-03-13 10:42:49 +0100

    Remove Getkw

--- cmake/custom/main.cmake
+++ cmake/custom/main.cmake
@@ -6,7 +6,6 @@ configure_file (
 set(PYTHON_SITE_INSTALL_DIR ${CMAKE_INSTALL_LIBDIR}/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/site-packages)
 
 # Fetch dependencies
-include(${PROJECT_SOURCE_DIR}/external/upstream/fetch_getkw.cmake)
 include(${PROJECT_SOURCE_DIR}/external/upstream/fetch_xcfun.cmake)
 include(${PROJECT_SOURCE_DIR}/external/upstream/fetch_eigen3.cmake)
 include(${PROJECT_SOURCE_DIR}/external/upstream/fetch_mrcpp.cmake)
