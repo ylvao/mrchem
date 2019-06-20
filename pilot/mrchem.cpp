276874e9e572fe22c9d1a2dcf6554d36d8c893dc
Stig Rune Jensen <stig.r.jensen@uit.no>
2019-06-20 11:38:51 +0200

    Add separate input parsing for pilot code

--- pilot/mrchem.cpp
+++ pilot/mrchem.cpp
@@ -7,21 +7,25 @@
 #include "mrenv.h"
 #include "parallel.h"
 
+// Initializing global variables
 mrcpp::MultiResolutionAnalysis<3> *mrchem::MRA;
 
-using namespace mrcpp;
+using json = nlohmann::json;
+using Timer = mrcpp::Timer;
 using namespace mrchem;
 
 int main(int argc, char **argv) {
-    mpi::initialize(argc, argv);
-    mrenv::initialize(argc, argv);
+    mpi::initialize();
+    const auto json_input = mrenv::fetch_input(argc, argv);
+    mrenv::initialize(json_input);
 
     Timer timer;
 
     // Do your stuff here
+    println(0, json_input.dump(2));
 
     timer.stop();
-    double wt = timer.getWallTime();
+    double wt = timer.elapsed();
 
     mrenv::finalize(wt);
     mpi::finalize();
