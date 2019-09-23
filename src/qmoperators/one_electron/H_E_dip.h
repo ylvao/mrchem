eb8a106f1f7b14fa2d4560c7a8310c2210c5c23e
Stig Rune Jensen <stig.r.jensen@uit.no>
2019-09-23 15:33:30 +0200

    Update H_E_dip

--- src/qmoperators/one_electron/H_E_dip.h
+++ src/qmoperators/one_electron/H_E_dip.h
@@ -1,38 +1,37 @@
 #pragma once
 
 #include "PositionOperator.h"
-#include "chemistry/Nucleus.h"
+#include "qmoperators/RankOneTensorOperator.h"
 
 /** Class H_E_dip
  *
  * @brief Electric dipole operator
  *
+ * mu_i = -r_i
+ *
  * The operator is based on the PositionOperator. The sign is coherent
- * with nuclear and electronic charges. It impleents explicit trace
- * functions for the nuclear contributions.
+ * with nuclear and electronic charges.
  *
  */
 
 namespace mrchem {
 
-/** @brief constructor
- *
- * @param[in] o the origin wrt which the dipole moment is computed.
- *
- */
-class H_E_dip final : public PositionOperator {
+class H_E_dip final : public RankOneTensorOperator<3> {
 public:
     H_E_dip(const mrcpp::Coord<3> &o)
-            : PositionOperator(o) {
+            : r(o) {
         // Invoke operator= to assign *this operator
         RankOneTensorOperator<3> &h = (*this);
-        h[0] = -1.0 * (*this)[0];
-        h[1] = -1.0 * (*this)[1];
-        h[2] = -1.0 * (*this)[2];
+        h[0] = -1.0 * r[0];
+        h[1] = -1.0 * r[1];
+        h[2] = -1.0 * r[2];
         h[0].name() = "h_E_dip[x]";
         h[1].name() = "h_E_dip[y]";
         h[2].name() = "h_E_dip[z]";
     }
+
+protected:
+    PositionOperator r;
 };
 
 } // namespace mrchem
