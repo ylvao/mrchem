b3188f73865120c1cebccdbcd282f9b3274fd296
Stig Rune Jensen <stig.r.jensen@uit.no>
2020-08-26 12:29:13 +0200

    Fix installation of SAD input files (#307)

--- cmake/custom/sad_basis.cmake
+++ cmake/custom/sad_basis.cmake
@@ -1,7 +1,7 @@
 set(SAD_BASIS_SOURCE_DIR ${PROJECT_SOURCE_DIR}/share/sad_basis CACHE STRING "Path to SAD basis and density files")
 set(SAD_BASIS_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/sad_basis)
 install(
-  DIRECTORY ${SAD_BASIS_DIR}
-  DESTINATION share/${PROJECT_NAME}/sad_basis
+  DIRECTORY share/sad_basis
+  DESTINATION share/${PROJECT_NAME}
   )
 
