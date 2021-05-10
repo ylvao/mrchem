6d968383eeccd231e359c6194625693f6b353505
Gabriel <gsa017@uit.no>
2021-05-10 11:57:33 +0200

    create CUBEfile class

---
+++ src/chemistry/CUBEfile.cpp
@@ -0,0 +1,39 @@
+/*
+ * MRChem, a numerical real-space code for molecular electronic structure
+ * calculations within the self-consistent field (SCF) approximations of quantum
+ * chemistry (Hartree-Fock and Density Functional Theory).
+ * Copyright (C) 2021 Stig Rune Jensen, Luca Frediani, Peter Wind and contributors.
+ *
+ * This file is part of MRChem.
+ *
+ * MRChem is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MRChem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MRChem.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For information on the complete list of contributors to MRChem, see:
+ * <https://mrchem.readthedocs.io/>
+ */
+#include "Cavity.h"
+#include "utils/math_utils.h"
+
+namespace mrchem {
+
+CUBEfile::CUBEfile() { }
+
+void CUBEfile::readFile() { }
+
+// Do a quadrature of the file
+double CUBEfile::evalf(const mrcpp::Coord<3> &r) const {
+}
+
+
+} // namespace mrchem
