ed83b58c0ebc47ced4beff3c152a15cddd1bd097
Stig Rune Jensen <stig.r.jensen@gmail.com>
2022-08-05 10:43:17 +0200

    Tweak Cavity print

--- src/environment/Permittivity.cpp
+++ src/environment/Permittivity.cpp
@@ -57,21 +57,21 @@ void Permittivity::printParameters() const {
     auto w0 = mrcpp::Printer::getWidth() - 1;
     auto w1 = 5;
     auto w2 = 9;
-    auto w3 = 2 * w0 / 9;
-    auto w4 = w0 - w1 - w2 - 3 * w3;
+    auto w3 = 6;
+    auto w4 = 10;
+    auto w5 = w0 - w1 - w2 - 3 * w3 - 3 * w4;
 
     // Build table column headers
     std::stringstream o_head;
     o_head << std::setw(w1) << "N";
-    o_head << std::setw(w2) << "Radius";
     o_head << std::setw(w2) << "R_0";
-    o_head << std::setw(w2) << "Alpha";
-    o_head << std::setw(w2) << "Beta";
-    o_head << std::setw(w2) << "Sigma";
-    o_head << std::string(w4 - 1, ' ') << ':';
-    o_head << std::setw(w3) << "x";
-    o_head << std::setw(w3) << "y";
-    o_head << std::setw(w3) << "z";
+    o_head << std::setw(w3+1) << "Alpha";
+    o_head << std::setw(w3-1) << "Beta";
+    o_head << std::setw(w3) << "Sigma";
+    o_head << std::setw(w5) << "Radius";
+    o_head << std::setw(w4) << "x";
+    o_head << std::setw(w4) << "y";
+    o_head << std::setw(w4) << "z";
 
     // Print
     mrcpp::print::header(0, "Solvation Cavity");
@@ -94,15 +94,14 @@ void Permittivity::printParameters() const {
 
         std::stringstream o_coord;
         o_coord << std::setw(w1) << i;
-        o_coord << std::setw(w2) << std::setprecision(4) << std::fixed << r;
         o_coord << std::setw(w2) << std::setprecision(4) << std::fixed << r_0;
-        o_coord << std::setw(w2) << std::setprecision(2) << std::fixed << alpha;
-        o_coord << std::setw(w2) << std::setprecision(2) << std::fixed << beta;
-        o_coord << std::setw(w2) << std::setprecision(2) << std::fixed << sigma;
-        o_coord << std::string(w4 - 1, ' ') << ':';
-        o_coord << std::setw(w3) << std::setprecision(6) << std::fixed << x;
-        o_coord << std::setw(w3) << std::setprecision(6) << std::fixed << y;
-        o_coord << std::setw(w3) << std::setprecision(6) << std::fixed << z;
+        o_coord << std::setw(w3) << std::setprecision(2) << std::fixed << alpha;
+        o_coord << std::setw(w3) << std::setprecision(2) << std::fixed << beta;
+        o_coord << std::setw(w3) << std::setprecision(2) << std::fixed << sigma << "  ->";
+        o_coord << std::setw(w5-4) << std::setprecision(4) << std::fixed << r;
+        o_coord << std::setw(w4) << std::setprecision(6) << std::fixed << x;
+        o_coord << std::setw(w4) << std::setprecision(6) << std::fixed << y;
+        o_coord << std::setw(w4) << std::setprecision(6) << std::fixed << z;
         println(0, o_coord.str());
     }
     mrcpp::print::separator(0, '=', 2);
