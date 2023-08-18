a8c224102ee1e1d4afa82f3493a37076eeafdf5e
moritzgubler <moritz.gubler@gmail.com>
2023-08-18 11:18:00 +0200

    merge historylist from initial repository. Uses now eigen block api instead of loops.

--- src/vc_sqnm/historylist.hpp
+++ src/vc_sqnm/historylist.hpp
@@ -25,39 +25,36 @@ namespace hlist_space{
     Eigen::MatrixXd normalized_difflist;
 
 
-    HistoryList(int &ndim_, int &nhistx_)
+    HistoryList(int ndim_, int nhistx_) : nhistx(nhistx_), ndim(ndim_)
     {
-      nhistx = nhistx_;
-      ndim = ndim_;
       hlist.resize(ndim, nhistx);
       difflist.resize(ndim, nhistx);
       normalized_difflist.resize(ndim, nhistx);
-      i_count = 0;
     }
 
-    int add(Eigen::VectorXd &x_in)
+    int add(const Eigen::VectorXd &x_in)
     {
       if (i_count < nhistx){
+        // list not yet full, append at the end.
         hlist.col(i_count) = x_in;
         i_count += 1;
-        for (int i = 1; i < i_count; i++){
-          difflist.col(i-1) = hlist.col(i) - hlist.col(i - 1);
-          normalized_difflist.col(i-1) = difflist.col(i-1) / difflist.col(i-1).norm();
+        // calculate difference list if we already have more than one element
+        if (i_count > 1)
+        {
+            difflist.block(0, 0, ndim, i_count - 1) = hlist.block(0, 1, ndim, i_count - 1) - hlist.block(0, 0, ndim, i_count -1);
+            normalized_difflist.block(0, 0, ndim, i_count - 1) = difflist.block(0, 0, ndim, i_count - 1).colwise().normalized();
         }
         return i_count - 1;
       } else {
+        // list is full, remove oldest element and shift all to new place.
         oldElem = hlist.col(0);
-        for (int i = 0; i < nhistx - 1; i++){
-          hlist.col(i) = hlist.col(i+1);
-        }
+        hlist.block(0, 0, ndim, nhistx - 1) = hlist.block(0, 1, ndim, nhistx - 1);
         hlist.col(nhistx - 1) = x_in;
         // calculate difference list
         difflist.col(0) = hlist.col(0) - oldElem;
         normalized_difflist.col(0) = difflist.col(0) / difflist.col(0).norm();
-        for (int i = 1; i < nhistx; i++){
-          difflist.col(i) = hlist.col(i) - hlist.col(i - 1);
-          normalized_difflist.col(i) = difflist.col(i) / difflist.col(i).norm();
-        }
+        difflist.block(0, 1, ndim, nhistx - 1) = hlist.block(0, 1, ndim, nhistx - 1) - hlist.block(0, 0, ndim, nhistx - 1);
+        normalized_difflist.block(0, 1, ndim, nhistx - 1) = difflist.block(0, 1, ndim, nhistx - 1).colwise().normalized();
         return nhistx;
         }
       }
