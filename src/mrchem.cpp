78622ba430fa736815fae7c2ca303ae58aab6251
moritzgubler <moritz.gubler@gmail.com>
2023-09-01 15:04:53 +0200

    write output json

--- src/mrchem.cpp
+++ src/mrchem.cpp
@@ -65,10 +65,13 @@ int main(int argc, char **argv) {
     PhysicalConstants::Initialize(con_inp);
     if (json_inp["printer"]["print_constants"]) PhysicalConstants::Print();
 
+    Timer timer;
+    json json_out;
+
     if (geopt_inp["run"]) {
-        optimize_positions(scf_inp, mol_inp, geopt_inp);
+        json_out = optimize_positions(scf_inp, mol_inp, geopt_inp);
+        mrcpp::mpi::barrier(mrcpp::mpi::comm_wrk);
     } else {
-        Timer timer;
         Molecule mol;
         driver::init_molecule(mol_inp, mol);
         auto scf_out = driver::scf::run(scf_inp, mol);
@@ -77,27 +80,26 @@ int main(int argc, char **argv) {
             for (auto &i : rsp_inp.items()) rsp_out[i.key()] = driver::rsp::run(i.value(), mol);
         }
         mrcpp::mpi::barrier(mrcpp::mpi::comm_wrk);
-        json json_out;
         // Name and version of the output schema
         json_out["schema_name"] = "mrchem_output";
         json_out["schema_version"] = 1;
-        // Provenance
-        json_out["provenance"] = {{"creator", "MRChem"},
-                                  {"version", program_version()},
-                                  {"nthreads", mrcpp::omp::n_threads},
-                                  {"mpi_processes", mrcpp::mpi::world_size},
-                                  {"total_cores", mrcpp::mpi::world_size * mrcpp::omp::n_threads},
-                                  {"routine", "mrchem.x"}};
         // Computed values
         json_out["scf_calculation"] = scf_out;
         json_out["rsp_calculations"] = rsp_out;
         json_out["properties"] = driver::print_properties(mol);
         // Global success field: true if all requested calculations succeeded
         json_out["success"] = detail::all_success(json_out);
-        mrenv::finalize(timer.elapsed());
-        mrenv::dump_json(json_inp, json_out);
     }
+    // Provenance
+    json_out["provenance"] = {{"creator", "MRChem"},
+                              {"version", program_version()},
+                              {"nthreads", mrcpp::omp::n_threads},
+                              {"mpi_processes", mrcpp::mpi::world_size},
+                              {"total_cores", mrcpp::mpi::world_size * mrcpp::omp::n_threads},
+                              {"routine", "mrchem.x"}};
 
+    mrenv::finalize(timer.elapsed());
+    mrenv::dump_json(json_inp, json_out);
     mrcpp::mpi::finalize();
     return EXIT_SUCCESS;
 }
