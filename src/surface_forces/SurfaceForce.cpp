b5de572fcefd568b320a4cdb841379f09cc3660a
Moritz Gubler <moritz.gubler@gmail.com>
2024-04-17 18:02:51 +0200

    maxwell test running

--- src/surface_forces/SurfaceForce.cpp
+++ src/surface_forces/SurfaceForce.cpp
@@ -26,75 +26,75 @@ using namespace mrchem;
 namespace surface_force {
 
 void plotRandomStuff(const mrchem::Molecule &mol, mrchem::OrbitalVector &Phi, double prec){
-    auto poisson_op = mrcpp::PoissonOperator(*mrchem::MRA, prec);
+    // auto poisson_op = mrcpp::PoissonOperator(*mrchem::MRA, prec);
 
-    int derivOrder = 1;
-    auto mrcd = std::make_shared<mrcpp::BSOperator<3>>(*mrchem::MRA, derivOrder);
-    // auto mrcd = std::make_shared<mrcpp::ABGVOperator<3>>(*MRA, 0.5, 0.5);
-    mrchem::NablaOperator nabla(mrcd);
-    nabla.setup(prec);
+    // int derivOrder = 1;
+    // auto mrcd = std::make_shared<mrcpp::BSOperator<3>>(*mrchem::MRA, derivOrder);
+    // // auto mrcd = std::make_shared<mrcpp::ABGVOperator<3>>(*MRA, 0.5, 0.5);
+    // mrchem::NablaOperator nabla(mrcd);
+    // nabla.setup(prec);
 
-    // mrcpp::ComplexFunction &rho;
-    mrchem::Density rho(false);
+    // // mrcpp::ComplexFunction &rho;
+    // mrchem::Density rho(false);
 
-    mrchem::density::compute(prec, rho, Phi, DensityType::Total);
+    // mrchem::density::compute(prec, rho, Phi, DensityType::Total);
 
-    // Adjust precision by system size
-    double abs_prec = prec / mrchem::orbital::get_electron_number(Phi);
+    // // Adjust precision by system size
+    // double abs_prec = prec / mrchem::orbital::get_electron_number(Phi);
 
-    mrcpp::ComplexFunction V(false);
-    V.alloc(mrchem::NUMBER::Real);
-    mrcpp::apply(abs_prec, V.real(), poisson_op, rho.real());
+    // mrcpp::ComplexFunction V(false);
+    // V.alloc(mrchem::NUMBER::Real);
+    // mrcpp::apply(abs_prec, V.real(), poisson_op, rho.real());
 
-    auto e_field = nabla(V);
+    // auto e_field = nabla(V);
 
-    double zmin = -.6;
-    double zmax = .6;
-    int n = 60;
-    auto r = mol.getNuclei()[0].getCoord();
+    // double zmin = -.6;
+    // double zmax = .6;
+    // int n = 60;
+    // auto r = mol.getNuclei()[0].getCoord();
 
-    r[0] = 0.0;
-    r[1] = 0.0;
-    int Z_k = 1;
+    // r[0] = 0.0;
+    // r[1] = 0.0;
+    // int Z_k = 1;
     
-    for (int i = 0; i < n + 1; i++)
-    {
-        double z = zmin + i * (zmax - zmin) / (n);
-        r[2] = z;
-        double c = 0.0001;
-        mrchem::NuclearGradientOperator h(Z_k, r, prec, c);
-        h.setup(prec);
-        auto e = h.trace(Phi).real();
-        // std::cerr << "e fielddd " << e[0] << " " << e[1] <<  " " << e[2] << std::endl;
-        // std::cerr << "nabla pot " << -o[0].real().evalf(r) << " " << -o[1].real().evalf(r) << " " << -o[2].real().evalf(r) << std::endl;
-        std:: cerr << r[2] << " " << e[2] << " " << - e_field[2].real().evalf(r) << " " << V.real().evalf(r) << std::endl;
-        h.clear();
-    }
+    // for (int i = 0; i < n + 1; i++)
+    // {
+    //     double z = zmin + i * (zmax - zmin) / (n);
+    //     r[2] = z;
+    //     double c = 0.0001;
+    //     mrchem::NuclearGradientOperator h(Z_k, r, prec, c);
+    //     h.setup(prec);
+    //     auto e = h.trace(Phi).real();
+    //     // std::cerr << "e fielddd " << e[0] << " " << e[1] <<  " " << e[2] << std::endl;
+    //     // std::cerr << "nabla pot " << -o[0].real().evalf(r) << " " << -o[1].real().evalf(r) << " " << -o[2].real().evalf(r) << std::endl;
+    //     std:: cerr << r[2] << " " << e[2] << " " << - e_field[2].real().evalf(r) << " " << V.real().evalf(r) << std::endl;
+    //     h.clear();
+    // }
     
-    // loop over circle in yz plane with radius 0.4 and center at (0,0,0.5)
-    double radius = 0.4;
+    // // loop over circle in yz plane with radius 0.4 and center at (0,0,0.5)
+    // double radius = 0.4;
 
-    std::ofstream outfile("toto");
-    double pi = 3.14159265359;
-    if (outfile.is_open()) {
-        for (int i = 0; i < n + 1; i++) {
-            r[1] = radius * std::cos(2 * pi * i / n);
-            r[2] = radius * std::sin(2 * pi * i / n) + 0.5;
-            double c = 0.0001;
-            mrchem::NuclearGradientOperator h(Z_k, r, prec, c);
-            h.setup(prec);
-            auto e = h.trace(Phi).real();
-            outfile << 2 * pi * i / n << " " << e[0] <<  " " << e[1] << " " << e[2] << " " << - e_field[0].real().evalf(r) << " " <<  - e_field[1].real().evalf(r) << " " << - e_field[2].real().evalf(r) << " " << std::endl;
-            h.clear();
-        }
-        outfile.close();
-    } else {
-        std::cerr << "Unable to open file" << std::endl;
-    }
+    // std::ofstream outfile("toto");
+    // double pi = 3.14159265359;
+    // if (outfile.is_open()) {
+    //     for (int i = 0; i < n + 1; i++) {
+    //         r[1] = radius * std::cos(2 * pi * i / n);
+    //         r[2] = radius * std::sin(2 * pi * i / n) + 0.5;
+    //         double c = 0.0001;
+    //         mrchem::NuclearGradientOperator h(Z_k, r, prec, c);
+    //         h.setup(prec);
+    //         auto e = h.trace(Phi).real();
+    //         outfile << 2 * pi * i / n << " " << e[0] <<  " " << e[1] << " " << e[2] << " " << - e_field[0].real().evalf(r) << " " <<  - e_field[1].real().evalf(r) << " " << - e_field[2].real().evalf(r) << " " << std::endl;
+    //         h.clear();
+    //     }
+    //     outfile.close();
+    // } else {
+    //     std::cerr << "Unable to open file" << std::endl;
+    // }
 }
 
 
-MatrixXd nuclearEfield(const MatrixXd nucPos, const VectorXd nucCharge, const MatrixXd& gridPos) {
+MatrixXd nuclearEfield(const MatrixXd &nucPos, const VectorXd &nucCharge, const MatrixXd gridPos) {
     int nGrid = gridPos.rows();
     int nNuc = nucPos.rows();
     MatrixXd Efield = MatrixXd::Zero(nGrid, 3);
@@ -112,14 +112,14 @@ MatrixXd nuclearEfield(const MatrixXd nucPos, const VectorXd nucCharge, const Ma
     return Efield;
 }
 
-mrcpp::ComplexFunction calcPotential(Density rho, mrcpp::PoissonOperator poisson, double prec) {
+mrcpp::ComplexFunction calcPotential(Density &rho, mrcpp::PoissonOperator &poisson, double prec) {
     mrcpp::ComplexFunction V(false);
     V.alloc(mrchem::NUMBER::Real);
     mrcpp::apply(prec, V.real(), poisson, rho.real());
     return V;
 }
 
-MatrixXd electronicEfield(mrcpp::ComplexFunction pot, NablaOperator nabla, const MatrixXd& gridPos) {
+MatrixXd electronicEfield(mrcpp::ComplexFunction &pot, NablaOperator &nabla, const MatrixXd &gridPos) {
     int nGrid = gridPos.rows();
     MatrixXd Efield = MatrixXd::Zero(nGrid, 3);
     auto e_field = nabla(pot);
@@ -133,7 +133,7 @@ MatrixXd electronicEfield(mrcpp::ComplexFunction pot, NablaOperator nabla, const
     return Efield;
 }
 
-Eigen::Tensor<double, 3> maxwellStress(const Molecule mol, mrcpp::ComplexFunction pot, NablaOperator nabla, const MatrixXd gridPos){
+Eigen::Tensor<double, 3> maxwellStress(const Molecule &mol, mrcpp::ComplexFunction &pot, NablaOperator &nabla, const MatrixXd &gridPos){
     int nGrid = gridPos.rows();
     int nNuc = mol.getNNuclei();
 
@@ -151,7 +151,7 @@ Eigen::Tensor<double, 3> maxwellStress(const Molecule mol, mrcpp::ComplexFunctio
     MatrixXd Efield = electronicEfield(pot, nabla, gridPos) + nuclearEfield(nucPos, nucCharge, gridPos);
 
     Eigen::Tensor<double, 3> stress(nGrid, 3, 3);
-    for (int i = 0; i < nNuc; i++) {
+    for (int i = 0; i < nGrid; i++) {
         for (int i1 = 0; i1 < 3; i1++) {
             for (int i2 = 0; i2 < 3; i2++) {
                 stress(i, i1, i2) = Efield(i, i1) * Efield(i, i2);
@@ -164,16 +164,53 @@ Eigen::Tensor<double, 3> maxwellStress(const Molecule mol, mrcpp::ComplexFunctio
             for (int i2 = 0; i2 < 3; i2++){
                 stress(i, i1, i2) *= 1.0 / (4 * M_PI);
             }
-        }   
+        }
     }
     return stress;
 }
 
 
+void testMaxwell(const mrchem::Molecule &mol, mrchem::OrbitalVector &Phi, double prec) {
+    auto poisson_op = mrcpp::PoissonOperator(*mrchem::MRA, prec);
+    int derivOrder = 1;
+    auto mrcd = std::make_shared<mrcpp::BSOperator<3>>(*mrchem::MRA, derivOrder);
+    mrchem::NablaOperator nabla(mrcd);
+    nabla.setup(prec);
+    mrchem::Density rho(false);
+    mrchem::density::compute(prec, rho, Phi, DensityType::Total);
+    double abs_prec = prec / mrchem::orbital::get_electron_number(Phi);
+    mrcpp::ComplexFunction pot = calcPotential(rho, poisson_op, abs_prec);
+
+
+
+    double zmin = -.6;
+    double zmax = .6;
+    int n = 60;
+    MatrixXd gridPos = MatrixXd::Zero(n , 3);
+    for (int i = 0; i < n; i++) {
+        double z = zmin + i * (zmax - zmin) / (n);
+        gridPos(i, 0) = 0.2;
+        gridPos(i, 1) = 0.2;
+        gridPos(i, 2) = z;
+    }
+    Tensor<double, 3> stress = maxwellStress(mol, pot, nabla, gridPos);
+
+    std::ofstream outfile("toto_stress");
+    for (int i = 0; i < n; i++)
+    {
+        outfile << gridPos(i, 2) << " " << stress(i, 0, 0) << " " << stress(i, 1, 1) << " " << stress(i, 2, 2) << " " << stress(i, 0, 1) << " " << stress(i, 0, 2) << " " << stress(i, 1, 2) << std::endl;
+        std::cerr << gridPos(i, 2) << " " << stress(i, 0, 0) << " " << stress(i, 1, 1) << " " << stress(i, 2, 2) << " " << stress(i, 0, 1) << " " << stress(i, 0, 2) << " " << stress(i, 1, 2) << std::endl;
+    
+    }
+    outfile.close();
+    
+}
+
 // Function definition
 std::vector<double> surface_forces(mrchem::Molecule &mol, mrchem::OrbitalVector &Phi, double prec) {
     std::cerr << "Surface force calculation" << std::endl;
-    plotRandomStuff(mol, Phi, prec);
+    // plotRandomStuff(mol, Phi, prec);
+    testMaxwell(mol, Phi, prec);
     std::vector<double> forceValues = {0.0, 0.0, 0.0};
     return forceValues;
 }
