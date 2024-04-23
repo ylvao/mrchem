9e8539a58b53d8ef9da3120dea98bc3bb7f50887
Moritz Gubler <moritz.gubler@gmail.com>
2024-04-23 13:46:37 +0200

    delete unused stuff

--- src/surface_forces/SurfaceForce.cpp
+++ src/surface_forces/SurfaceForce.cpp
@@ -38,75 +38,6 @@ using nlohmann::json;
 
 namespace surface_force {
 
-void plotRandomStuff(const mrchem::Molecule &mol, mrchem::OrbitalVector &Phi, double prec){
-    // auto poisson_op = mrcpp::PoissonOperator(*mrchem::MRA, prec);
-
-    // int derivOrder = 1;
-    // auto mrcd = std::make_shared<mrcpp::BSOperator<3>>(*mrchem::MRA, derivOrder);
-    // // auto mrcd = std::make_shared<mrcpp::ABGVOperator<3>>(*MRA, 0.5, 0.5);
-    // mrchem::NablaOperator nabla(mrcd);
-    // nabla.setup(prec);
-
-    // // mrcpp::ComplexFunction &rho;
-    // mrchem::Density rho(false);
-
-    // mrchem::density::compute(prec, rho, Phi, DensityType::Total);
-
-    // // Adjust precision by system size
-    // double abs_prec = prec / mrchem::orbital::get_electron_number(Phi);
-
-    // mrcpp::ComplexFunction V(false);
-    // V.alloc(mrchem::NUMBER::Real);
-    // mrcpp::apply(abs_prec, V.real(), poisson_op, rho.real());
-
-    // auto e_field = nabla(V);
-
-    // double zmin = -.6;
-    // double zmax = .6;
-    // int n = 60;
-    // auto r = mol.getNuclei()[0].getCoord();
-
-    // r[0] = 0.0;
-    // r[1] = 0.0;
-    // int Z_k = 1;
-    
-    // for (int i = 0; i < n + 1; i++)
-    // {
-    //     double z = zmin + i * (zmax - zmin) / (n);
-    //     r[2] = z;
-    //     double c = 0.0001;
-    //     mrchem::NuclearGradientOperator h(Z_k, r, prec, c);
-    //     h.setup(prec);
-    //     auto e = h.trace(Phi).real();
-    //     // std::cerr << "e fielddd " << e[0] << " " << e[1] <<  " " << e[2] << std::endl;
-    //     // std::cerr << "nabla pot " << -o[0].real().evalf(r) << " " << -o[1].real().evalf(r) << " " << -o[2].real().evalf(r) << std::endl;
-    //     std:: cerr << r[2] << " " << e[2] << " " << - e_field[2].real().evalf(r) << " " << V.real().evalf(r) << std::endl;
-    //     h.clear();
-    // }
-    
-    // // loop over circle in yz plane with radius 0.4 and center at (0,0,0.5)
-    // double radius = 0.4;
-
-    // std::ofstream outfile("toto");
-    // double pi = 3.14159265359;
-    // if (outfile.is_open()) {
-    //     for (int i = 0; i < n + 1; i++) {
-    //         r[1] = radius * std::cos(2 * pi * i / n);
-    //         r[2] = radius * std::sin(2 * pi * i / n) + 0.5;
-    //         double c = 0.0001;
-    //         mrchem::NuclearGradientOperator h(Z_k, r, prec, c);
-    //         h.setup(prec);
-    //         auto e = h.trace(Phi).real();
-    //         outfile << 2 * pi * i / n << " " << e[0] <<  " " << e[1] << " " << e[2] << " " << - e_field[0].real().evalf(r) << " " <<  - e_field[1].real().evalf(r) << " " << - e_field[2].real().evalf(r) << " " << std::endl;
-    //         h.clear();
-    //     }
-    //     outfile.close();
-    // } else {
-    //     std::cerr << "Unable to open file" << std::endl;
-    // }
-}
-
-
 MatrixXd nuclearEfield(const MatrixXd &nucPos, const VectorXd &nucCharge, const MatrixXd gridPos) {
     int nGrid = gridPos.rows();
     int nNuc = nucPos.rows();
@@ -306,7 +237,6 @@ std::vector<Matrix3d> kineticStress(const mrchem::Molecule &mol, mrchem::Orbital
     return stress;
 }
 
-
 /**
  * Calculates the forces using surface integrals for a given molecule and orbital vector.
  *
@@ -361,6 +291,5 @@ Eigen::MatrixXd surface_forces(mrchem::Molecule &mol, mrchem::OrbitalVector &Phi
     
     return forces;
 }
-
 
 } // namespace surface_force
