a3319d5fa7bb1248e4453acf6a8595ce7b2de34f
moritzgubler <moritz.gubler@gmail.com>
2024-05-29 16:58:16 +0200

    install azora files

---
+++ cmake/custom/azora_potentials.cmake
@@ -0,0 +1,7 @@
+set(SAD_BASIS_SOURCE_DIR ${PROJECT_SOURCE_DIR}/share/azora_potentials CACHE STRING "Path to azora potentials")
+set(SAD_BASIS_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/azora_potentials)
+install(
+  DIRECTORY share/azora_potentials
+  DESTINATION share/${PROJECT_NAME}
+  )
+
