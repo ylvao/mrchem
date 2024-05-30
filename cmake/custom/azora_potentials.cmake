7b00b37e220063a4d4787b7dee11d5715248a8d8
moritzgubler <moritz.gubler@gmail.com>
2024-05-30 08:58:04 +0200

    correct name of variable

--- cmake/custom/azora_potentials.cmake
+++ cmake/custom/azora_potentials.cmake
@@ -1,5 +1,5 @@
-set(SAD_BASIS_SOURCE_DIR ${PROJECT_SOURCE_DIR}/share/azora_potentials CACHE STRING "Path to azora potentials")
-set(SAD_BASIS_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/azora_potentials)
+set(AZORA_POTENTIALS_SOURCE_DIR ${PROJECT_SOURCE_DIR}/share/azora_potentials CACHE STRING "Path to azora potentials")
+set(AZORA_POTENTIALS_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/azora_potentials)
 install(
   DIRECTORY share/azora_potentials
   DESTINATION share/${PROJECT_NAME}
