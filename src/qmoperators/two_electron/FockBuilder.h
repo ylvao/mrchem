1bcf590d51eb091f57f3e458b00ce358b8366399
moritzgubler <moritz.gubler@gmail.com>
2024-08-19 11:41:08 +0200

    rename kappa to chi

--- src/qmoperators/two_electron/FockBuilder.h
+++ src/qmoperators/two_electron/FockBuilder.h
@@ -105,8 +105,8 @@ private:
     std::shared_ptr<XCOperator> xc{nullptr};
     std::shared_ptr<ReactionOperator> Ro{nullptr};       // Reaction field operator
     std::shared_ptr<ElectricFieldOperator> ext{nullptr}; // Total external potential
-    std::shared_ptr<ZoraOperator> kappa{nullptr};
-    std::shared_ptr<ZoraOperator> kappa_inv{nullptr};
+    std::shared_ptr<ZoraOperator> chi{nullptr};
+    std::shared_ptr<ZoraOperator> chi_inv{nullptr};
 
     std::shared_ptr<QMPotential> collectZoraBasePotential();
     OrbitalVector buildHelmholtzArgumentZORA(OrbitalVector &Phi, OrbitalVector &Psi, DoubleVector eps, double prec);
