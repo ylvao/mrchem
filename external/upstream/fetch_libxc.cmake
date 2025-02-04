86557f74c7d9cb3a53af5efa10397a1cc43b511d
ylvao <ylvaos1@gmail.com>
2025-02-04 14:17:20 +0100

    some formatting

--- external/upstream/fetch_libxc.cmake
+++ external/upstream/fetch_libxc.cmake
@@ -1,13 +1,4 @@
 cmake_policy(SET CMP0144 NEW)
-
-# add_library(Libxc::xc STATIC)
-# set_target_properties(Libxc::xc PROPERTIES
-    # IMPORTED_LOCATION "/home/ylva/work/libxc-mrchem/libxc/libxc_install/lib/libxc.a"
-# )
-
-
-# set(export LIBXC_DIR=/home/ylva/work/libxc-mrchem/libxc/libxc_install)
-
 find_package(Libxc CONFIG QUIET
 NO_CMAKE_PATH
 NO_CMAKE_PACKAGE_REGISTRY
@@ -27,10 +18,10 @@ else()
 
   FetchContent_GetProperties(Libxc)
 
-set(CMAKE_BUILD_TYPE Release)
-set(ENABLE_TESTALL FALSE CACHE BOOL "")
-set(LIBXC_MAX_ORDER 3)  # TODO Maybe as a user-facing option?
-set(LIBXC_PYTHON_INTERFACE FALSE CACHE BOOL "")
+  set(CMAKE_BUILD_TYPE Release)
+  set(ENABLE_TESTALL FALSE CACHE BOOL "")
+  set(LIBXC_MAX_ORDER 3)  # TODO Maybe as a user-facing option?
+  set(LIBXC_PYTHON_INTERFACE FALSE CACHE BOOL "")
 
   if(NOT libxc_sources_POPULATED)
     FetchContent_Populate(Libxc)
