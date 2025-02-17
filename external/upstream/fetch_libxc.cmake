a49fae405463e3cee0a79d446bea27b71de5a0fd
ylvao <ylvaos1@gmail.com>
2025-02-17 12:41:49 +0100

    Small changes for testing

--- external/upstream/fetch_libxc.cmake
+++ external/upstream/fetch_libxc.cmake
@@ -1,34 +1,39 @@
+set(Libxc_pinned "7.0.0")
 cmake_policy(SET CMP0144 NEW)
-find_package(Libxc CONFIG QUIET
+find_package(Libxc CONFIG QUIET COMPONENTS C
 NO_CMAKE_PATH
 NO_CMAKE_PACKAGE_REGISTRY
 NO_CMAKE_SYSTEM_PACKAGE_REGISTRY
 )
-if(TARGET Libxc::xc)
-  get_property(_loc TARGET Libxc::xc PROPERTY LOCATION)
-  message(STATUS "Found LibXC: ${_loc} (found version ${Libxc_VERSION})")
-else()
-  message(STATUS "Suitable LibXC could not be located. Fetching and building!")
-  include(FetchContent)
-  FetchContent_Declare(Libxc
-    QUIET
-    URL
-      https://gitlab.com/libxc/libxc/-/archive/7.0.0/libxc-7.0.0.tar.gz
-    )
+# if(TARGET Libxc::xc)
+#   get_property(_loc TARGET Libxc::xc PROPERTY LOCATION)
+#   message(STATUS "Found LibXC: ${_loc} (found version ${Libxc_VERSION})")
+# else()
+message(STATUS "Suitable Libxc could not be located. Fetching and building!")
+include(FetchContent)
+FetchContent_Declare(Libxc
+  QUIET
+  DOWNLOAD_EXTRACT_TIMESTAMP ON
+  URL
+    https://gitlab.com/libxc/libxc/-/archive/${Libxc_pinned}/libxc-${Libxc_pinned}.tar.gz
+  )
 
-  FetchContent_GetProperties(Libxc)
+enable_language(C)
 
-  set(CMAKE_BUILD_TYPE Release)
-  set(ENABLE_TESTALL FALSE CACHE BOOL "")
-  set(LIBXC_MAX_ORDER 3)  # TODO Maybe as a user-facing option?
-  set(LIBXC_PYTHON_INTERFACE FALSE CACHE BOOL "")
+# compile 3rd derivative code
+set(DISABLE_KXC OFF CACHE BOOL "" FORCE)
+# compile 4th derivative code
+set(DISABLE_LXC OFF CACHE BOOL "" FORCE)
+# disable compilation of testing infrastructure
+set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+# only build shared libraries
+set(BUILD_SHARED_LIBS ON CACHE BOOL "" FORCE)
+# enable arch-dependent flags if they are used for VeloxChem
+set(ENABLE_XHOST ${ENABLE_ARCH_FLAGS} CACHE BOOL "" FORCE)
 
-  if(NOT libxc_sources_POPULATED)
-    FetchContent_Populate(Libxc)
+FetchContent_MakeAvailable(Libxc)
 
-    add_subdirectory(
-      ${libxc_sources_SOURCE_DIR}
-      ${libxc_sources_BINARY_DIR}
-      )
-  endif()
-endif()
+# Provide an alias, so linking to Libxc looks the same regardless if it was
+# found on the system or if it was fetched at configuration
+add_library(Libxc::xc ALIAS xc)
+# endif()
