6459cb06c71b6301b4d1727133fc0e7e5f36a4f0
Niklas Goellmann <ngoellmann@gmail.com>
2026-06-04 14:39:24 +0200

    new mrcpp version

--- external/upstream/fetch_mrcpp.cmake
+++ external/upstream/fetch_mrcpp.cmake
@@ -39,7 +39,7 @@ else()
     GIT_REPOSITORY
     https://github.com/msnik1999/mrcpp.git
     GIT_TAG
-    5e6035252ca71b42bab0b484d688db8f2d564761
+    f2afeee65065a9e98146b56741d106394a952cbc
   )
 
   FetchContent_GetProperties(mrcpp_sources)
